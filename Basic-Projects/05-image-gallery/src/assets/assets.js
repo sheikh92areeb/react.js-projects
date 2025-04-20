1e496b3fb1c86271ea6d2ae2f8e9c8bbfb5ddf45
Areeb Sheikh <mareebsheikh92@gmail.com>
2025-04-21 01:27:24 +0500

    updated

---
+++ Basic-Projects/05-image-gallery/src/assets/assets.js
@@ -0,0 +1,33 @@
+import pic1 from './1.jpg'
+import pic2 from './2.jpg'
+import pic3 from './3.jpg'
+import pic4 from './4.jpg'
+import pic5 from './5.jpg'
+import pic6 from './6.jpg'
+import pic7 from './7.jpg'
+import pic8 from './8.jpg'
+import pic9 from './9.jpg'
+import pic10 from './10.jpg'
+import pic11 from './11.jpg'
+import pic12 from './12.jpg'
+import pic13 from './13.jpg'
+import pic14 from './14.jpg'
+import pic15 from './15.jpg'
+
+export const assets = [
+    {title:'Landscape' , src:pic1, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic2, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic3, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic4, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic5, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic6, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic7, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic8, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic9, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic10, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic11, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic12, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic13, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic14, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'},
+    {title:'Landscape' , src:pic15, alt:'landscape', desc:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime doloribus tempora odio tenetur nihil in officiis sed aperiam natus magni.'}
+]
--- Basic-Projects/05-image-gallery/src/components/ImageCard.jsx
+++ Basic-Projects/05-image-gallery/src/components/ImageCard.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function ImageCard() {
+function ImageCard({assets}) {
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
   return (
-    <div>
-      
-    </div>
+    <>
+      {assets.map((image, index) => (
+        <div key={index} className='card border p-2 rounded-lg bg-white'>
+          <div className="card-image">
+            <img src={image.src} alt={image.alt} className='w-full h-[200px] object-cover rounded-lg' />
+          </div>
+          <div className="card-text mt-5 p-3">
+            <h2 className='text-xl font-medium text-gray-500 mb-1'>{image.title}</h2>
+            <p className='text-sm mb-5'>{image.desc}</p>
+            <button  onClick={() => setSelectedImage(image.src)} className='border px-2 py-1 rounded-full cursor-pointer hover:bg-blue-200'>View Image</button>
+          </div>
+        </div>
+      ))}
+
+      {selectedImage && (
+        <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
+          <div className="bg-white p-6 rounded-lg shadow-lg relative">
+            <button className="absolute top-1 right-1 text-gray-600 hover:text-black cursor-pointer" onClick={() => setSelectedImage(null)}>
+              ❌
+            </button>
+            <img src={selectedImage} alt="Zoomed" className="max-w-full max-h-[80vh]" />
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
